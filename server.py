0a1e161d2b1ecc5a390d6ed65d57a2d366b085d7
Marius <sarmaniucm@gmail.com>
2021-03-01 01:15:08 +0200

    something

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from sklearn import linear_model, preprocessing
 from sklearn.model_selection import train_test_split
 import pandas
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 regr = None
@@ -26,17 +26,33 @@ def train():
     regr = linear_model.LinearRegression()
     regr.fit(X_train, y_train)
 
-    prediction = regr.predict(X_test)
-    print("ORDINARY LEAST SQUARES")
-    a = pandas.DataFrame({"Prediction": prediction, "Actual": y_test})
-    print(a.head())
-    print('Score: %.2f' % regr.score(X_test, y_test))
-    print("===================================")
+    # prediction = regr.predict(X_test)
+    # print("ORDINARY LEAST SQUARES")
+    # print(X_test)
+    # a = pandas.DataFrame({"Prediction": prediction, "Actual": y_test})
+    # print(a.head())
+    # print('Score: %.2f' % regr.score(X_test, y_test))
+    # print("===================================")
 
 
 @app.route('/')
 def render_main_page():
     return render_template("index.html")
+
+
+@app.route('/predict', methods=["POST"])
+def predict_price():
+    user_data = request.form.to_dict()
+    complexAge = float(user_data["complexAge"])
+    totalRooms = float(user_data["totalRooms"])
+    totalBedrooms = float(user_data["totalBedrooms"])
+    complexInhabitants = float(user_data["complexInhabitants"])
+    apartmentsNr = float(user_data["apartmentsNr"])
+    data = [[complexAge, totalRooms, totalBedrooms, complexInhabitants, apartmentsNr]]
+    data = preprocessing.normalize(data, norm='l2')
+
+    x = regr.predict(data)
+    return str(x)
 
 
 if __name__ == "__main__":
