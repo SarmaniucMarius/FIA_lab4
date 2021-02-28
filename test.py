631636e43dc52614c099cc7bd00f3526ee775975
Marius <sarmaniucm@gmail.com>
2021-03-01 01:49:06 +0200

    corrected yml

--- test.py
+++ test.py
@@ -5,4 +5,3 @@ def test_training():
     regr, X_test, y_test = train(["complexAge", "totalRooms", "totalBedrooms", "complexInhabitants", "apartmentsNr", "medianCompexValue"])
 
     assert regr.score(X_test, y_test) >= 0.21
-    assert False
