25bc2ed418c931c90c474b4f4c9d82184de909df
Marius <sarmaniucm@gmail.com>
2021-03-01 01:46:40 +0200

    testing tests

--- test.py
+++ test.py
@@ -5,3 +5,4 @@ def test_training():
     regr, X_test, y_test = train(["complexAge", "totalRooms", "totalBedrooms", "complexInhabitants", "apartmentsNr", "medianCompexValue"])
 
     assert regr.score(X_test, y_test) >= 0.21
+    assert False
